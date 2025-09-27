daf899385dd03bbb78b66aa352517d680b0d828e
Stephanie Crich <stephicrich@gmail.com>
2025-09-27 14:00:19 +0100

    Initial commit

---
+++ src/main/java/uk/ac/ed/acp/cw2/configuration/IlpRestServiceConfig.java
@@ -0,0 +1,16 @@
+package uk.ac.ed.acp.cw2.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import uk.ac.ed.acp.cw2.data.RuntimeEnvironment;
+
+import java.net.URL;
+
+@Configuration
+@EnableScheduling
+public class IlpRestServiceConfig {
+
+
+}
---
+++ src/main/java/uk/ac/ed/acp/cw2/controller/ServiceController.java
@@ -0,0 +1,41 @@
+package uk.ac.ed.acp.cw2.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.*;
+import uk.ac.ed.acp.cw2.data.RuntimeEnvironment;
+
+import java.net.URL;
+import java.time.Instant;
+
+/**
+ * Controller class that handles various HTTP endpoints for the application.
+ * Provides functionality for serving the index page, retrieving a static UUID,
+ * and managing key-value pairs through POST requests.
+ */
+@RestController()
+@RequestMapping("/api/v1")
+public class ServiceController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceController.class);
+
+    @Value("${ilp.service.url}")
+    public URL serviceUrl;
+
+
+    @GetMapping("/")
+    public String index() {
+        return "<html><body>" +
+                "<h1>Welcome from ILP</h1>" +
+                "<h4>ILP-REST-Service-URL:</h4> <a href=\"" + serviceUrl + "\" target=\"_blank\"> " + serviceUrl+ " </a>" +
+                "</body></html>";
+    }
+
+    @GetMapping("/uid")
+    public String uid() {
+        return "s12345678";
+    }
+}
